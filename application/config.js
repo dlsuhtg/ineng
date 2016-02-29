dace4795b0f9819417578423559efebfef9a3552
dlsuhtg <harlequintheatreguild@gmail.com>
2016-02-29 10:29:03 +0800

    Updated URL links
    Updated to master copy sheet link 
    by: Pat Mananghaya

--- application/config.js
+++ application/config.js
@@ -7,10 +7,10 @@ Giving it a value of 1 means you are allowing it to be shown on the pages the sp
 On the other hand, giving it a value of 0 or any other value will make it unaccessible.
 */
 
-var ticketmonitoringsheet = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=2073713539";
+var ticketmonitoringsheet = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=419113379";
 var lockticketmonitoringsheet = 1;
 
-var incentivesmasterlist = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=2073713539";
+var incentivesmasterlist = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=419113379";
 var lockincentivesmasterlist = 1;
 
 var professorsincentiveslist = "https://docs.google.com/spreadsheets/d/1w6vEK4sd2AS5nsJCmpu0Cy7LESz4Y-IvhmIVMZP-kqQ/edit#gid=0";
