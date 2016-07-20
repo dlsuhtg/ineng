7b63ce2a2c71f482670943c969d282d02fe20db6
dlsuhtg <harlequintheatreguild@gmail.com>
2016-07-20 10:04:20 +0800

    Updated links for TMS, Incentives list and Professors list
    By: Patricia Mananghaya

--- application/config.js
+++ application/config.js
@@ -10,10 +10,10 @@ On the other hand, giving it a value of 0 or any other value will make it unacce
 var ticketmonitoringsheet = "https://docs.google.com/spreadsheets/d/1F-R_d_7d0NQmViqsh3-lCwttaTNzPJTJSh7xVY4O1HQ/edit#gid=1960300865";
 var lockticketmonitoringsheet = 1;
 
-var incentivesmasterlist = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=419113379";
+var incentivesmasterlist = "https://docs.google.com/spreadsheets/d/1F-R_d_7d0NQmViqsh3-lCwttaTNzPJTJSh7xVY4O1HQ/edit#gid=1960300865";
 var lockincentivesmasterlist = 1;
 
-var professorsincentiveslist = "https://docs.google.com/spreadsheets/d/1w6vEK4sd2AS5nsJCmpu0Cy7LESz4Y-IvhmIVMZP-kqQ/edit#gid=0";
+var professorsincentiveslist = "https://docs.google.com/spreadsheets/d/1jo0kSUSMisuyOrn3TjZtLdusf39eqqKJDsBuNF2hdGw/edit#gid=0";
 var lockprofessorsincentiveslist = 1;
 
 /*PDF LETTERS*/
