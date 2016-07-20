a9808c3616328bbceefc0e4440014f7fa7dafe3c
dlsuhtg <harlequintheatreguild@gmail.com>
2016-07-20 09:58:41 +0800

    Updated Ticket Monitoring Sheet for Kabilang Ibayo
    By: Patricia Mananghaya

--- application/config.js
+++ application/config.js
@@ -7,7 +7,7 @@ Giving it a value of 1 means you are allowing it to be shown on the pages the sp
 On the other hand, giving it a value of 0 or any other value will make it unaccessible.
 */
 
-var ticketmonitoringsheet = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=419113379";
+var ticketmonitoringsheet = "https://docs.google.com/spreadsheets/d/1F-R_d_7d0NQmViqsh3-lCwttaTNzPJTJSh7xVY4O1HQ/edit#gid=1960300865";
 var lockticketmonitoringsheet = 1;
 
 var incentivesmasterlist = "https://docs.google.com/spreadsheets/d/1wrfv8NY041PQlq84OT92psO0OC0yI1vOHatIplqZBH4/edit#gid=419113379";
